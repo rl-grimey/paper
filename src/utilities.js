e41b30cc0ff68dc6ef72fcc14a36a9d74d71a249
Larson <rlrson@uiowa.edu>
2018-07-05 21:33:16 -0500

    Add weeks to utilities, adjust color scale

--- src/utilities.js
+++ src/utilities.js
@@ -7,7 +7,7 @@ import { scaleOrdinal, scaleLinear } from 'd3';
 export const communities =  [-1, 0, 1, 2, 3, 4];
 export const community_scale = scaleOrdinal()
   .domain(communities)
-  .range(['#bdbdbd', '#7fc97f','#ffff99','#fdc086', '#beaed4', '#e31a1c']);
+  .range(['#bdbdbd', '#7fc97f','#ffff99','#fdc086', '#beaed4', '#ef3b2c']);
 export const community_labels = scaleOrdinal()
   .domain(communities)
   .range([
@@ -23,17 +23,15 @@ export const community_labels = scaleOrdinal()
 
 
 
-
-
-
-
-
-
-  /* Styles */
+/* Styles */
 export const center_styles = {
   'alignItems': 'center',
-  'justifyContent': 'center'
+  'justifyContent': 'center',
+  'margin': '3px'
 };
+
+/* Weeks */
+export const weeks = [-4, -3, -2, -1, 0, 1, 2, 3, 4];
 
 
 //export const aspect_ratio = (width, height) => {}
