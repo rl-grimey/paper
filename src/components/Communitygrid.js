f59ae7f3bb525b40d03428f02b3594c33d5f7c54
Larson <rlrson@uiowa.edu>
2018-07-05 20:34:56 -0500

    Create inter-topic cloud scales

--- src/components/Communitygrid.js
+++ src/components/Communitygrid.js
@@ -1,8 +1,8 @@
 /* Dependencies */
 import React from 'react';
 import { Row } from 'react-bootstrap';
-import { scaleBand } from '@vx/scale';
-import { entries } from 'd3';
+import { scaleLinear, scaleSqrt, scaleQuantile, scaleBand } from 'd3';
+import { entries, extent } from 'd3';
 import CommunityCloud from './tiles/CommunityCloud';
 import { communities, center_styles } from '../utilities';
 
@@ -22,6 +22,8 @@ export default class CommunityGrid extends React.Component {
 
     this.create_clouds_scale = this.create_clouds_scale.bind(this);
     this.create_cloud = this.create_cloud.bind(this);
+    this.create_cloud_data = this.create_cloud_data.bind(this);
+    this.create_intercloud_scale = this.create_intercloud_scale.bind(this);
   }
 
   componentDidMount() {
@@ -48,21 +50,57 @@ export default class CommunityGrid extends React.Component {
 
   create_clouds_scale(width) {
     /* Creates a scale to fit our 6 communities reasonably. */
-    return scaleBand({
+    /*return scaleBand({
       domain: communities,
       range: [0, width],
       padding: 0.1
-    });
+    });*/
+    return scaleBand()
+      .domain(communities)
+      .range([0, width])
+      .padding(0.1);
   }
 
-  create_cloud(community, i) {
+  create_cloud_data(community) {
+    /* Extracts the top 20 keywords for each topic community */
+    // Conditionally set the view, based on absolute (count) or relative (rank)
+    let view_attr = (this.state.view === 'absolute') ? 'docs' : 'rank';
+
+    // Select top 20 and format
+    let top_20 = community.slice(0, 20).map(d => {
+      return { text: d.word, value: d[view_attr] };
+    });
+
+    return top_20;
+  }
+
+  create_intercloud_scale() {
+    /* Creates a size scale based on values from all community tokens. */
+    //let intercloud_scale = scaleLinear().range([8, 28]);
+    //let intercloud_scale = scaleSqrt().range([8, 28]);
+    let intercloud_scale = scaleQuantile().range([10, 14, 18, 22, 28]);
+    
+    if (this.state.data === {}) return intercloud_scale;
+    else {
+      // Concatentate the top 20 tokens for each community
+      let top_20s = [].concat.apply([], entries(this.state.data)
+        .map(d => this.create_cloud_data(d.value)))
+        .map(d => d.value);
+
+      intercloud_scale.domain(extent(top_20s));
+      return intercloud_scale;
+    }
+  }
+
+  create_cloud(community, key, scale) {
     let data = community.value;
     let this_community = community.key;
 
     return (<CommunityCloud
-      key={i}
+      key={key}
       width={this.state.scale.bandwidth()}
       height={this.state.height}
+      scale={scale}
       view={this.state.view}
       community={this_community}
       selected_community={this.state.community}
@@ -72,11 +110,13 @@ export default class CommunityGrid extends React.Component {
   }
 
   render() {
+    let intercloud = this.create_intercloud_scale();
+
     return (
       <Row style={center_styles}>{this.state.data && 
         entries(this.state.data)
           .filter(d => +d.key !== 5) // Filter out protests
-          .map((d, i) => this.create_cloud(d, i))
+          .map((d, i) => this.create_cloud(d, i, intercloud))
       }</Row>
     );
   }
