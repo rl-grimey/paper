d22527f09ee9881ea575a9846cb197fbc322b4d8
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-10-25 21:47:47 -0500

    Use separate resize and chart dimension functions in app

--- src/app.js
+++ src/app.js
@@ -56,18 +56,41 @@ class App extends React.Component {
 
   handleResize() {
     /* Adjust width to maximum possible after the component mounts. */
-    // Div dimensions
+    // App dimensions
     let app_div = document.querySelector('#app_ref');
     let app_width = app_div.clientWidth;
+    let app_height = app_div.clientHeight;
 
     // Window dimensions
     let window_width = window.innerWidth;
     let window_height = window.innerHeight;
 
     // Calculations to preserve a fixed aspect ratio
-    //let min_dim = Math.min(window_width, window_height);
+    this.setState({ width: app_width, height: window_height });
+  }
 
-    this.setState({ width: window_width, height: window_height });
+  getDimensions = () => {
+    // Initial dimensions are taken from state, which is in turn set by the window dimensions.
+    let { width: width, height: total_height } = this.state;
+
+    // Get the toolbar's height. If we're rendering for our first time, initialize
+    let toolbar_div = document.querySelector('#Toolbar');
+    let tool_height = (toolbar_div !== null) ? toolbar_div.clientHeight : 125;
+
+    // Get the remaining height and portion it out by the 2/3s rule
+    let canvas_height = total_height - tool_height;
+    let cloud_height = Math.round(canvas_height * 0.25);
+    let carto_height = Math.round(canvas_height * 0.75);
+
+    // Get the cartogram's width, while preserving it's aspect ratio (11:8)
+    const carto_ratio = 11/8;
+    const carto_width = Math.round(carto_height * carto_ratio);
+
+    return {
+      cloud_height,
+      carto_height,
+      carto_width
+    };
   }
 
   handleEscape(event) {
@@ -105,7 +128,15 @@ class App extends React.Component {
     //console.log('/* App ---------------------------------------------------*/');
     //console.log(this.state);
 
-    let cloud_tile_height = Math.min(this.state.height / 4, 275);
+    let upper_third = this.state.height / 3;
+    let toolbar_height = upper_third * 0.3333;
+    let cloud_tile_height = upper_third * 0.6666;
+
+    //let cloud_tile_height = Math.min(this.state.height / 4, 275);
+
+
+    // Dimensions
+    const { cloud_height, carto_height, carto_width } = this.getDimensions();
 
     return (
       <div className="App">
@@ -120,9 +151,10 @@ class App extends React.Component {
           <hr/>
 
           <div id='figureDiv'>
-            {this.state.chart === 'topics' && <CommunityGrid
+            {this.state.chart === 'topics' && 
+            <CommunityGrid
               width={this.state.width}
-              height={cloud_tile_height}
+              height={cloud_height}
               community={this.state.community}
               view={this.state.view}
               onClickCloud={this.handleClickCloud}
@@ -130,8 +162,8 @@ class App extends React.Component {
 
             <Row style={center_styles}>
               <Cartogrid
-                width={this.state.width}
-                height={this.state.height}
+                width={carto_width}
+                height={carto_height}
                 padding={this.state.padding}
                 chart={this.state.chart}
                 view={this.state.view}
